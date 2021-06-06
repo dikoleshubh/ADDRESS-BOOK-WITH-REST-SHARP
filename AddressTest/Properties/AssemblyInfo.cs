c83c51994c8af8ba3fc4eb504cb1d7e6ef627257
dikoleshubh <dikole.shubh@gmail.com>
2021-06-06 13:56:56 +0530

    Ability to Add Entry

---
+++ AddressTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AddressTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AddressTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4294fde0-433a-4df9-95b4-0f74ba68279e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AddressTest/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using RestSharp;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net;
+using AddressRestSharp;
+
+namespace AddressTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+       
+            RestClient client;
+
+            [TestInitialize]
+            public void SetUp()
+            {
+                //Initialize the base URL to execute requests made by the instance
+                client = new RestClient("http://localhost:3000");
+            }
+            private IRestResponse GetContactList()
+            {
+                //Arrange
+                //Initialize the request object with proper method and URL
+                RestRequest request = new RestRequest("contacts", Method.GET);
+                //Act
+                // Execute the request
+                RestSharp.IRestResponse response = client.Execute(request);
+                return response;
+            }
+            
+            // UC1 Ability to read the entries from json server.
+            
+            [TestMethod]
+            public void ReadEntriesFromJsonServer()
+            {
+                IRestResponse response = GetContactList();
+                /// Check if the status code of response equals the default code for the method requested
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+                /// Convert the response object to list of employees
+                List<Contact> employeeList = JsonConvert.DeserializeObject<List<Contact>>(response.Content);
+                Assert.AreEqual(5, employeeList.Count);
+                foreach (Contact c in employeeList)
+                {
+                    Console.WriteLine($"Id: {c.Id}\tFullName: {c.FirstName} {c.LastName}\tPhoneNo: {c.PhoneNumber}\tAddress: {c.Address}\tCity: {c.City}\tState: {c.State}\tZip: {c.Zip}\tEmail: {c.Email}");
+                }
+            }
+     
+}
+        
+    
+
+
+    
